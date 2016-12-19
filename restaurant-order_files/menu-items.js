c22984bb4ae2b610a1394f51e67df88819f9d5df
Geoffrey Litt <glitt@panoramaed.com>
2016-12-19 16:25:27 -0500

    basic display of items working

---
+++ restaurant-order_files/menu-items.js
@@ -0,0 +1,33 @@
+// Item names for the various sections of the demo restaurant menu
+var MENU_ITEMS = {
+  "Sandwiches": [
+    "Club",
+    "Reuben",
+    "Turkey",
+    "Ham",
+    "BLT",
+    "Cheese",
+    "Vegetarian",
+    "Meatball"
+  ],
+  "Sides": [
+    "Salad",
+    "Chips",
+    "Apple",
+    "Orange"
+  ],
+  "Drinks": [
+    "Cola",
+    "Juice",
+    "Coffee",
+    "Water"
+  ]
+};
+
+
+// Min and max prices for each category
+var PRICES = {
+  "Sandwiches": [7, 15],
+  "Sides": [3, 7],
+  "Drinks": [2, 5]
+};
